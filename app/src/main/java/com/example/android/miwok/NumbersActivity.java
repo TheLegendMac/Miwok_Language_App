b0cb3a2f47c3bd5235a8cf25471e38c1cf023d29
Mac Taylor <gamingmac@outlook.com>
2018-02-11 00:07:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        //Declaring array for the english words
+        String [] words = new String[10];
+        //Initializing the elements of the array
+        words[0] = "One";
+        words[1] = "Two";
+        words[2] = "Three";
+        words[3] = "Four";
+        words[4] = "Five";
+        words[5] = "Six";
+        words[6] = "Seven";
+        words[7] = "Eight";
+        words[8] = "Nine";
+        words[9] = "Ten";
+
+        Log.v("Numbers Activity", "English Number Array created");
+    }
+
+}
